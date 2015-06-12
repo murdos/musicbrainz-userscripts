74800b0c5ef9c00b883c93f0c6186ac984b10762
Laurent Monin <zas@norz.org>
2015-06-13 00:31:10 +0200

    encyclopedisque_importer: use relative paths for libs

--- encyclopedisque_importer.user.js
+++ encyclopedisque_importer.user.js
@@ -8,8 +8,8 @@
 // @include        http://www.encyclopedisque.fr/disque/*.html
 // @include        http://www.encyclopedisque.fr/artiste/*.html
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
-// @require        https://raw.github.com/murdos/musicbrainz-userscripts/master/lib/import_functions.js
-// @require        https://raw.github.com/murdos/musicbrainz-userscripts/master/lib/logger.js
+// @require        lib/import_functions.js
+// @require        lib/logger.js
 // ==/UserScript==
 
 $(document).ready(function() {
