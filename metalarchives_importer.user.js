4825b1020761d4d3e426b7e08fb9a18f56b9c43a
Maxime <max190690@hotmail.fr>
2015-08-23 20:35:38 +0200

    beautify
    Beautify source code with http://jsbeautifier.org/

--- metalarchives_importer.user.js
+++ metalarchives_importer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		Import Metal Archives releases into MB
 // @namespace	https://github.com/murdos/musicbrainz-userscripts/
-// @version		2015.08.23.2
+// @version		2015.08.23.4
 // @description	Add a button on Metal Archives release pages allowing to open MusicBrainz release editor with pre-filled data for the selected release
 // @downloadURL	https://raw.github.com/murdos/musicbrainz-userscripts/master/metalarchives_importer.user.js
 // @update		https://raw.github.com/murdos/musicbrainz-userscripts/master/metalarchives_importer.user.js
@@ -10,106 +10,113 @@
 // @require		lib/mbimport.js
 // @require    	lib/logger.js
 // ==/UserScript==
-$(document).ready(function () {
+$(document).ready(function() {
     var release = retrieveReleaseInfo();
     insertLink(release);
-	LOGGER.info("Parsed release: ", release);
+    LOGGER.info("Parsed release: ", release);
 });
 
 function setreleasedate(release, datestring) {
     var patt = new RegExp('^d{4}$');
     if (patt.exec(datestring)) {
         release.year = datestring;
-    } else if (datestring.indexOf(',') != - 1) {
+    } else if (datestring.indexOf(',') != -1) {
         var commaindex = datestring.indexOf(',');
         var d = new Date(datestring.substring(0, commaindex - 2) + datestring.substring(commaindex));
         release.year = d.getFullYear();
-        release.month = d.getMonth()+1;
+        release.month = d.getMonth() + 1;
         release.day = d.getDate();
-    } else{
-        var d = new Date("2 "+datestring);
+    } else {
+        var d = new Date("2 " + datestring);
         release.year = d.getFullYear();
-        release.month = d.getMonth()+1;
+        release.month = d.getMonth() + 1;
     }
     return release;
 }
 
-function getGenericalData(){
+function getGenericalData() {
     var rdata = new Array();
-    var keydata = $('dl.float_left dt, dl.float_right dt').map(function(){
+    var keydata = $('dl.float_left dt, dl.float_right dt').map(function() {
         var s = $.trim($(this).text());
-        return s.substring(0,s.length-1);
-    } ).get();
-    var valuedata= $('dl.float_left dd,dl.float_right dd').map(function(){
+        return s.substring(0, s.length - 1);
+    }).get();
+    var valuedata = $('dl.float_left dd,dl.float_right dd').map(function() {
         return $.trim($(this).text());
-    } ).get();
-    for(i=0;i<keydata.length;i++){
-        rdata[keydata[i]]=valuedata[i];
+    }).get();
+    for (i = 0; i < keydata.length; i++) {
+        rdata[keydata[i]] = valuedata[i];
     }
     return rdata;
 }
+
 function retrieveReleaseInfo() {
     var release = new Object();
-    release.artist_credit =new Array();
-    release.artist_credit.push({artist_name: $('h2.band_name').text(),credited_name: $('h2.band_name').text()});
+    release.artist_credit = new Array();
+    release.artist_credit.push({
+        artist_name: $('h2.band_name').text(),
+        credited_name: $('h2.band_name').text()
+    });
     release.title = $('h1.album_name').text();
 
-    var rdata=getGenericalData();
+    var rdata = getGenericalData();
 
     release = setreleasedate(release, rdata["Release date"]);
     //todo add case for multiple labels if such a case exist
     release.labels = new Array();
-    release.labels.push( { 
-        name: (rdata["Label"] == "Independent"?"[no label]":rdata["Label"]), 
-        catno: (rdata["Catalog ID"] == "N/A" ? "" :rdata["Catalog ID"]) 
-    } );
+    release.labels.push({
+        name: (rdata["Label"] == "Independent" ? "[no label]" : rdata["Label"]),
+        catno: (rdata["Catalog ID"] == "N/A" ? "" : rdata["Catalog ID"])
+    });
 
-    release.type =ReleaseTypes[rdata["Type"]][0];
+    release.type = ReleaseTypes[rdata["Type"]][0];
     release.secondary_types = new Array(ReleaseTypes[rdata["Type"]][1]);
     release.status = 'official';
-	
-	rdata.hasOwnProperty("Version desc.") &&  rdata["Version desc."].indexOf("Digipak")!=-1?release.packaging ="Digipak":release.packaging ="";
 
-    var identifiers=$("#album_tabs_notes > div:nth-child(2)").find("p:not([class])").contents();
-    for(var j=0;j<identifiers.length;j++){
-        if(identifiers[j].textContent.indexOf("Barcode:")!=-1){
-            release.barcode=$.trim(identifiers[j].textContent.substring(8));
+    rdata.hasOwnProperty("Version desc.") && rdata["Version desc."].indexOf("Digipak") != -1 ? release.packaging = "Digipak" : release.packaging = "";
+
+    var identifiers = $("#album_tabs_notes > div:nth-child(2)").find("p:not([class])").contents();
+    for (var j = 0; j < identifiers.length; j++) {
+        if (identifiers[j].textContent.indexOf("Barcode:") != -1) {
+            release.barcode = $.trim(identifiers[j].textContent.substring(8));
             break;
         }
     }
 
     // Release URL
-	  // URLs
+    // URLs
     var link_type = MBImport.URL_TYPES;
     release.urls = new Array();
-    release.urls.push( { url: window.location.href, link_type: link_type.other_databases } );
+    release.urls.push({
+        url: window.location.href,
+        link_type: link_type.other_databases
+    });
 
-    var releaseNumber=1;
+    var releaseNumber = 1;
     release.discs = new Array();
     release.discs.push(new Object());
-    release.discs[releaseNumber-1].tracks=new Array();
-    release.discs[releaseNumber-1].format=ReleaseFormat[rdata["Format"]];
-    var tracksline= $('table.table_lyrics tr.even,table.table_lyrics tr.odd');
-    var trackslinelength=tracksline.length;
-   
-    tracksline.each(function(index,element){
-        var trackNumber= $.trim(element.children[0].textContent).replace('.',"");
-        if( trackNumber == "1" && trackNumber != index+1 ){
+    release.discs[releaseNumber - 1].tracks = new Array();
+    release.discs[releaseNumber - 1].format = ReleaseFormat[rdata["Format"]];
+    var tracksline = $('table.table_lyrics tr.even,table.table_lyrics tr.odd');
+    var trackslinelength = tracksline.length;
+
+    tracksline.each(function(index, element) {
+        var trackNumber = $.trim(element.children[0].textContent).replace('.', "");
+        if (trackNumber == "1" && trackNumber != index + 1) {
             releaseNumber++;
             release.discs.push(new Object());
-            release.discs[releaseNumber-1].tracks = new Array();
-            release.discs[releaseNumber-1].format=ReleaseFormat[rdata["Format"]];
+            release.discs[releaseNumber - 1].tracks = new Array();
+            release.discs[releaseNumber - 1].format = ReleaseFormat[rdata["Format"]];
         }
-        
+
         var track = {
-            'number':trackNumber,
+            'number': trackNumber,
             'title': element.children[1].textContent,
             'duration': element.children[2].textContent,
             'artist_credit': [release.artist_credit]
         };
-        release.discs[releaseNumber-1].tracks.push(track);
+        release.discs[releaseNumber - 1].tracks.push(track);
     });
-    return release;   
+    return release;
 }
 
 // Insert links in page
@@ -120,7 +127,7 @@ function insertLink(release) {
     var innerHTML = MBImport.buildFormHTML(parameters);
 
     $('h2.band_name').append(innerHTML);
-    $("form[action='//musicbrainz.org/release/add']").css("padding","initial");
+    $("form[action='//musicbrainz.org/release/add']").css("padding", "initial");
 
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -133,36 +140,36 @@ on MA				on MB
 Full-length 		Album				Compilation		                  
 Live album			Single				Demo
 Demo				EP					DJ-mix
-Single				Broadcast	Interview
-EP					Other		Live
-Video							Audiobook
-Boxed set							Mixtape/Street
-Split								Remix
-Video/VHS (legacy)					Soundtrack
-Compilation							Spokenword
+Single				Broadcast			Interview
+EP					Other				Live
+Video									Audiobook
+Boxed set								Mixtape/Street
+Split									Remix
+Video/VHS (legacy)						Soundtrack
+Compilation								Spokenword
 Split video
 */
 //ReleaseTypes[MAtype]=["primary type","secondary type on mb"];
 var ReleaseTypes = new Array();
-ReleaseTypes["Full-length"]=["Album"];
-ReleaseTypes["Live album"]=["Album", "Live"];
-ReleaseTypes["Demo"]=["Album","Demo"];
-ReleaseTypes["Single"]=["Single"];
-ReleaseTypes["EP"]=["EP"];
-ReleaseTypes["Compilation"]=["Album", "Compilation"];
-ReleaseTypes["Split"]=["Album"];
+ReleaseTypes["Full-length"] = ["Album"];
+ReleaseTypes["Live album"] = ["Album", "Live"];
+ReleaseTypes["Demo"] = ["Album", "Demo"];
+ReleaseTypes["Single"] = ["Single"];
+ReleaseTypes["EP"] = ["EP"];
+ReleaseTypes["Compilation"] = ["Album", "Compilation"];
+ReleaseTypes["Split"] = ["Album"];
 
 //ReleaseFormat[MAformat]="MBformat";
 var ReleaseFormat = new Array();
-ReleaseFormat["CD"]="CD";
-ReleaseFormat["2CD"]="CD";
-ReleaseFormat["Vinyl"]="Vinyl";
-ReleaseFormat["7\" vinyl"]="7\" Vinyl";
-ReleaseFormat["7\" vinyl (33⅓ RPM)"]="7\" Vinyl";
-ReleaseFormat["10\" vinyl (33⅓ RPM)"]="10\" Vinyl";
-ReleaseFormat["10\" vinyl"]="10\" Vinyl";
-ReleaseFormat["12\" vinyl"]="12\" Vinyl";
-ReleaseFormat["2 12\" vinyls"]="12\" Vinyl";
-ReleaseFormat["12\" vinyl (33⅓ RPM)"]="12\" Vinyl";
-ReleaseFormat["Cassette"]="Cassette";
-ReleaseFormat["Digital"]="Digital Media";
+ReleaseFormat["CD"] = "CD";
+ReleaseFormat["2CD"] = "CD";
+ReleaseFormat["Vinyl"] = "Vinyl";
+ReleaseFormat["7\" vinyl"] = "7\" Vinyl";
+ReleaseFormat["7\" vinyl (33⅓ RPM)"] = "7\" Vinyl";
+ReleaseFormat["10\" vinyl (33⅓ RPM)"] = "10\" Vinyl";
+ReleaseFormat["10\" vinyl"] = "10\" Vinyl";
+ReleaseFormat["12\" vinyl"] = "12\" Vinyl";
+ReleaseFormat["2 12\" vinyls"] = "12\" Vinyl";
+ReleaseFormat["12\" vinyl (33⅓ RPM)"] = "12\" Vinyl";
+ReleaseFormat["Cassette"] = "Cassette";
+ReleaseFormat["Digital"] = "Digital Media";
