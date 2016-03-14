0ca46a81dde1bfae4867d69f0f7520b4941640a3
madmouser1 <lenjoubert@gmail.com>
2016-03-14 12:17:50 +0000

    JS Formatting corrected and artistitem lowercase implemented for switch

--- takealot.user.js
+++ takealot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Import Takealot releases to MusicBrainz
 // @description    Add a button to import Takealot releases to MusicBrainz
-// @version        2016.03.09.1
+// @version        2016.03.09.2
 // @namespace      http://lenjoubertblog.blogspot.com
 // @include        http*://www.takealot.com/*
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
@@ -13,9 +13,6 @@
 // @grant          none
 // ==/UserScript==
 
-// need to do button and paste next
-// needto look at artist it picked up track1 as the artist name.
-
 
 // prevent JQuery conflicts, see http://wiki.greasespot.net/@grant
 this.$ = this.jQuery = jQuery.noConflict(true);
@@ -23,39 +20,41 @@ this.$ = this.jQuery = jQuery.noConflict(true);
 if (!unsafeWindow) unsafeWindow = window;
 
 var DEBUG = true;
-//DEBUG = true;
+//DEBUG = false;
 if (DEBUG) {
-  LOGGER.setLevel('debug');
+	LOGGER.setLevel('debug');
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /*
  * Test cases:
- * - http://www.takealot.com/theuns-jordaan-roeper-cd/PLID17284867
+ * - http://www.takealot.com/theuns-jordaan-roeper-cd/PLID17284867 - working
+ * - http://www.takealot.com/various-artists-still-the-one-3cd/PLID40723650 - pending
  */
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 $(document).ready(function() {
 	LOGGER.info("Document Ready & Takealot Userscript executing");
-	var TakealotRelease = ParseTakealotPage();	
+	var TakealotRelease = ParseTakealotPage();
 	insertMBSection(TakealotRelease);
 });
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//                             Insert MusicBrainz section into Discogs page                                           //
+//                             Insert MusicBrainz section into Takealot page                                          //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function insertMbUI(mbUI) {
-  var e;
-  if ((e = $("div.section.box-summary")) && e.length) {
-    e.after(mbUI);
-  } else if ((e = $('#buybox')) && e.length) {
-    e.before(mbUI);
-  } else if ((e = $("div.section.more-choices")) && e.length) {
-    e.before(mbUI);
-  }
+	var e;
+	if ((e = $("div.section.box-summary")) && e.length) {
+		e.after(mbUI);
+	} else if ((e = $('#buybox')) && e.length) {
+		e.before(mbUI);
+	} else if ((e = $("div.section.more-choices")) && e.length) {
+		e.before(mbUI);
+	}
 }
 
 
@@ -63,49 +62,50 @@ function insertMbUI(mbUI) {
 
 // Insert links in Takealot page
 function insertMBSection(release) {
-   LOGGER.debug("insertMBsection Firing");
+	LOGGER.debug("insertMBsection Firing");
 
-    var mbUI = $('<div class="section musicbrainz"><h3>MusicBrainz</h3></div>').hide();
+	var mbUI = $('<div class="section musicbrainz"><h3>MusicBrainz</h3></div>').hide();
 
-    if (DEBUG) mbUI.css({'border': '1px dotted red'});
+	if (DEBUG) mbUI.css({
+		'border': '1px dotted red'
+	});
 
-    var mbContentBlock = $('<div class="section_content"></div>');
-    mbUI.append(mbContentBlock);
+	var mbContentBlock = $('<div class="section_content"></div>');
+	mbUI.append(mbContentBlock);
 
-    if (release.maybe_buggy) {
-      var warning_buggy = $('<p><small><b>Warning</b>: this release has perhaps a buggy tracklist, please check twice the data you import.</small><p').css({'color': 'red', 'margin-top': '4px', 'margin-bottom': '4px'});
-      mbContentBlock.prepend(warning_buggy);
-    }
+	// Form parameters
+	var edit_note = MBImport.makeEditNote(window.location.href, 'Takealot');
+	LOGGER.debug("*** Edit Note: ", edit_note);
+	var parameters = MBImport.buildFormParameters(release, edit_note);
+	LOGGER.debug("*** Form parameters: ",parameters);
+	// Build form + search button
+	var innerHTML = '<div id="mb_buttons">' + MBImport.buildFormHTML(parameters) + MBImport.buildSearchButton(release) + '</div>';
+	mbContentBlock.append(innerHTML);
 
-    // Form parameters
-    var edit_note = MBImport.makeEditNote(window.location.href, 'Takealot');
-	  LOGGER.debug("*** Edit Note: ",edit_note);
-    var parameters = MBImport.buildFormParameters(release, edit_note);
-   LOGGER.debug("***Form parameters");
-    // Build form + search button
-    var innerHTML = '<div id="mb_buttons">'
-      + MBImport.buildFormHTML(parameters)
-      + MBImport.buildSearchButton(release)
-      + '</div>';
-    mbContentBlock.append(innerHTML);
+	insertMbUI(mbUI);
 
-    insertMbUI(mbUI);
+	$('#mb_buttons').css({
+		display: 'inline-block',
+		width: '100%'
+	});
+	$('form.musicbrainz_import').css({
+		width: '49%',
+		display: 'inline-block'
+	});
+	$('form.musicbrainz_import_search').css({
+		'float': 'right'
+	})
+	$('form.musicbrainz_import > button').css({
+		width: '100%',
+		'box-sizing': 'border-box'
+	});
 
-    $('#mb_buttons').css({
-      display: 'inline-block',
-      width: '100%'
-    });
-    $('form.musicbrainz_import').css({width: '49%', display:'inline-block'});
-    $('form.musicbrainz_import_search').css({'float': 'right'})
-    $('form.musicbrainz_import > button').css({width: '100%', 'box-sizing': 'border-box'});
-
-    mbUI.slideDown();
+	mbUI.slideDown();
 }
 
 
-
 // Analyze Takealot data and return a release object
-function ParseTakealotPage () {
+function ParseTakealotPage() {
 	LOGGER.debug("ParseTakealotPage function firing");
 
 	var releasebarcode = "";
@@ -117,139 +117,139 @@ function ParseTakealotPage () {
 	// sample header "Huisgenoot Se 20 Country-Treffers - Various Artists (CD)"
 	var MediaHeading = document.querySelectorAll("h1.fn");
 	LOGGER.debug(MediaHeading[0].innerText);
-    var TitleStr = MediaHeading[0].innerText;
+	var TitleStr = MediaHeading[0].innerText;
 	var TitleRegex = /(.*)-(.*)+\s\(([^)]+)\)/;
 	var HeadArray = TitleStr.match(TitleRegex);
 	releasetitle = HeadArray[1];
-	LOGGER.debug("** Title from H1** ",releasetitle);
+	LOGGER.debug("** Title from H1** ", releasetitle);
 	// need to move and do check further down sometimes artist sometimes album first
 	// therefore if title = artist move to next item in array ... a real hack
-	
-	
+
+
 	// Select all DL data in the "Product Info" div id = second div class = details 
 	var allinfolist = document.querySelectorAll("div#second > div.details > dl > *");
 
 	// Iterate all over the lines
 	for (var i = 0; i < allinfolist.length; i++) {
-	    var artistitemlabel = allinfolist[i];
+		var artistitemlabel = allinfolist[i];
 
-	    if (artistitemlabel.tagName == "DT") {
+		if (artistitemlabel.tagName == "DT") {
+			var artistitem = artistitemlabel.textContent.toLowerCase();
+			switch (artistitem) {
+				case "barcode": // use these cases to select the spesific text values
+					releasebarcode = artistitemlabel.nextSibling.textContent.trim();
+					LOGGER.debug('The value is :' + artistitem + ' > ' + releasebarcode);
+					break;
+				case "country": // use these cases to select the spesific text values
+					releasecountry = artistitemlabel.nextSibling.textContent.trim();
+					LOGGER.debug('The value is :' + artistitem + ' > ' + releasecountry);
+					break;
+				case "date released": // use these cases to select the spesific text values
+					releasedaterel = artistitemlabel.nextSibling.textContent.trim();
+					LOGGER.debug('The value is :' + artistitem + ' > ' + releasedaterel);
+					break;
+				case "language": // use these cases to select the spesific text values
+					releaselanguage = artistitemlabel.nextSibling.textContent.trim();
+					LOGGER.debug('The value is :' + artistitem + ' > ' + releaselanguage);
+					break;
+				case "title": // use these cases to select the spesific text values
+					releasetitle = artistitemlabel.nextSibling.textContent.trim();
+					LOGGER.debug('The value is :' + artistitem + ' > ' + releasetitle);
+					break;
+				case "tracks": // use these cases to select the spesific text values
+					LOGGER.debug('The label chosen is :' + artistitem);
 
-	        var artistitem = artistitemlabel.textContent;
+					// Iterate over all the tracks - changed * to li to try and only catch tracks
+					var alltracklist = document.querySelectorAll("div#second > div.details > dl > dd > ol:last-child > li");
+					LOGGER.debug(" *** Dump the tracks nodeList to see what is going on ***");
+					LOGGER.debug(alltracklist);
+					// Tracks
+					var tracklistarray = new Array(); // create the tracklist array to use later
 
-	        switch (artistitem) {
-			case "Barcode":
-	        case "BarCode": // use these cases to select the spesific text values
-	            //LOGGER.info('The label chosen is :' + artistitem);
-	            releasebarcode = artistitemlabel.nextSibling.textContent.trim();
-	            LOGGER.debug('The value is :' + releasebarcode);
-	            break;
-	        case "Country": // use these cases to select the spesific text values
-	            //LOGGER.info('The label chosen is :' + artistitem);
-	            releasecountry = artistitemlabel.nextSibling.textContent.trim();
-	            LOGGER.debug('The value is :' + releasecountry);
-	            break;
-	        case "Date Released": // use these cases to select the spesific text values
-	            //LOGGER.info('The label chosen is :' + artistitem);
-	            releasedaterel = artistitemlabel.nextSibling.textContent.trim();
-	            LOGGER.debug('The value is :' + releasedaterel);
-	            break;
-	        case "Language": // use these cases to select the spesific text values
-	            //LOGGER.info('The label chosen is :' + artistitem);
-	            releaselanguage = artistitemlabel.nextSibling.textContent.trim();
-	            LOGGER.debug('The value is :' + releaselanguage);
-	            break;
-	        case "Title": // use these cases to select the spesific text values
-	            //LOGGER.info('The label chosen is :' + artistitem);
-	            releasetitle = artistitemlabel.nextSibling.textContent.trim();
-	            LOGGER.debug('The value is :' + releasetitle);
-	            break;
-	        case "Tracks": // use these cases to select the spesific text values
-	            LOGGER.debug('The label chosen is :' + artistitem);
+					//var releaseartist = alltracklist[0].textContent.trim();
+					// The format on Takealot changed and Artist is not the first element in li anymore but last
+					// remember this is a nodeList and not an array
+					var releaseartist = alltracklist[alltracklist.length - 1].textContent.trim();
+					LOGGER.debug('The album artist:' + releaseartist);
+					for (var j = 0; j < alltracklist.length - 1; j++) { // start at 1 to ignore 0 as it is not a track but previous ol need to change code to only iterate second ol
+						// changed j to 0 and length-1 as Artist is at end now
+						var track = new Object();
+						//LOGGER.info('The track number:' + j);
+						var trackdetails = alltracklist[j].textContent.trim();
+						//LOGGER.info('The track name:' + trackdetails);
+						var getridoffront = trackdetails.replace(/\[ Disc 01 Track./g, ""); //regex to change beginning of text
+						var changebacktodot = getridoffront.replace(/\b\d{2}.\]./g, ""); // regex to change the remainder of text in correct MusicBrainz import format   new \b\d{2}.\]  old .\].
+						//LOGGER.debug('The track regexed:' + changebacktodot);
 
-	            // Iterate over all the tracks - changed * to li to try and only catch tracks
-	            var alltracklist = document.querySelectorAll("div#second > div.details > dl > dd > ol:last-child > li");
-	            LOGGER.debug(" *** Dump the tracks array to see what is going on ***");
-				LOGGER.debug(alltracklist);    
-	            // Tracks
-	            var tracklistarray = new Array(); // create the tracklist array to use later
-	                
-	            //var releaseartist = alltracklist[0].textContent.trim();
-							// The format on Takealot changed and Artist is not the first element in li anymore but last
-							// remember this is a nodeList and not an array
-	            var releaseartist = alltracklist[alltracklist.length-1].textContent.trim();
-	            LOGGER.debug('The album artist:' + releaseartist);
-	            for (var j = 0; j < alltracklist.length-1; j++) { // start at 1 to ignore 0 as it is not a track but previous ol need to change code to only iterate second ol
-	                // changed j to 0 and length-1 as Artist is at end now
-								  var track = new Object();
-	                //LOGGER.info('The track number:' + j);
-	                var trackdetails = alltracklist[j].textContent.trim();
-	                //LOGGER.info('The track name:' + trackdetails);
-	                var getridoffront = trackdetails.replace(/\[ Disc 01 Track./g, ""); //regex to change beginning of text
-	                var changebacktodot = getridoffront.replace(/\b\d{2}.\]./g, ""); // regex to change the remainder of text in correct MusicBrainz import format   new \b\d{2}.\]  old .\].
-	                //LOGGER.debug('The track regexed:' + changebacktodot);
+						track.number = j;
+						track.title = changebacktodot;
+						tracklistarray.push(track); // push the track objects into the array
 
-	            	track.number = j;
-	            	track.title = changebacktodot;
-	                tracklistarray.push(track); // push the track objects into the array
-
-	            }
-	            //LOGGER.info(tracklistarray);
-	            break;
-	        }
-	    }
+					}
+					//LOGGER.info(tracklistarray);
+					break;
+			}
+		}
 	}
 
 	release = new Object();
-    
-    // Release artist credit
-    release.artist_credit = new Array();
 
-    var artist_name = releaseartist;
-    release.artist_credit.push( { 'artist_name': artist_name } );
-    
-    // Release title
-    release.title = releasetitle;
+	// Release artist credit
+	release.artist_credit = new Array();
+
+	var artist_name = releaseartist;
+	release.artist_credit.push({
+		'artist_name': artist_name
+	});
+
+	// Release title
+	release.title = releasetitle;
 	// Release Barcode
-    release.barcode = releasebarcode;
-    
-    // Default status is official
-    release.status = 'official';
-    
-    // Other hard-coded info
-    release.script = 'Latn';
-    
-    release.country = Countries[ releasecountry ];
-    release.language = Languages[releaselanguage];
-    
-    var disc = {'position': 1, 'tracks': tracklistarray }; //use the tracklist array
-    release.discs = [ disc ];
-    
-    release.labels = [];
-    
-    // Release URL
-    release.urls = new Array();
-    release.urls.push( { 'url': window.location.href, 'link_type': 74 } ); //type 74 is purchase for download
-    
-        // Release date
-        var releasedate = releasedaterel;
-        if (typeof releasedate != "undefined" && releasedate != "") {
-            var tmp = releasedate.split('-');        if (tmp[0] != "undefined" && tmp[0] != "") {
-                release.year = parseInt(tmp[0], 10);
-                if (tmp[1] != "undefined" && tmp[1] != "") {
-                    release.month = parseInt(tmp[1], 10);
-                    if (tmp[2] != "undefined" && tmp[2] != "") {
-                        release.day = parseInt(tmp[2], 10);
-                    }
-                }
-            }
-        }
-    
-    LOGGER.info("Release:",release);
+	release.barcode = releasebarcode;
 
-    return release;
+	// Default status is official
+	release.status = 'official';
+
+	// Other hard-coded info
+	release.script = 'Latn';
+
+	release.country = Countries[releasecountry];
+	release.language = Languages[releaselanguage];
+
+	var disc = {
+		'position': 1,
+		'tracks': tracklistarray
+	}; //use the tracklist array
+	release.discs = [disc];
+
+	release.labels = [];
+
+	// Release URL
+	release.urls = new Array();
+	release.urls.push({
+		'url': window.location.href,
+		'link_type': 74
+	}); //type 74 is purchase for download
+
+	// Release date
+	var releasedate = releasedaterel;
+	if (typeof releasedate != "undefined" && releasedate != "") {
+		var tmp = releasedate.split('-');
+		if (tmp[0] != "undefined" && tmp[0] != "") {
+			release.year = parseInt(tmp[0], 10);
+			if (tmp[1] != "undefined" && tmp[1] != "") {
+				release.month = parseInt(tmp[1], 10);
+				if (tmp[2] != "undefined" && tmp[2] != "") {
+					release.day = parseInt(tmp[2], 10);
+				}
+			}
+		}
+	}
+
+	LOGGER.info("Release:", release);
+
+	return release;
 }
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                   Takealot -> MusicBrainz mapping                                                  //
