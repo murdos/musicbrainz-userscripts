74cb1df783d3fcf4b5ca88a8d982cfa3a10adc3f
David Logie <djl@mksh.org>
2020-07-08 17:30:55 +0100

    mb_ui_enhancements: Add a-tisket links next to Deezer, Spotify, iTunes, Apple Music links.

--- mb_ui_enhancements.user.js
+++ mb_ui_enhancements.user.js
@@ -272,6 +272,37 @@ $(document).ready(function () {
         $('ul.tabs').append(`<li><a href="/release/${mbid}/edit-relationships">Edit relationships</a></li>`);
     }
 
+    // Display a-tisket links next to Deezer, Spotify, iTunes and Apple Music links
+    re = new RegExp('musicbrainz.org/release/([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})', 'i');
+    if (window.location.href.match(re)) {
+        document.querySelectorAll('div#bottom-credits a').forEach(function (link) {
+            if (link.href.match(/deezer.com|(music|itunes).apple.com|spotify.com/)) {
+                var id;
+                var fragment;
+
+                if (link.href.match(/deezer.com/)) {
+                    id = new URL(link.href).pathname.split('/').slice(-1)[0];
+                    fragment = 'deez';
+                } else if (link.href.match(/apple.com/)) {
+                    id = new URL(link.href).pathname.split('/', 5).slice(-1)[0].replace('id', '');
+                    fragment = 'itu';
+                } else if (link.href.match(/spotify.com/)) {
+                    id = new URL(link.href).pathname.split('/', 5).slice(-1)[0];
+                    fragment = 'spf';
+                }
+
+                var next = link.nextElementSibling.nextElementSibling;
+                var newlink = document.createElement('a');
+                newlink.href = `https://etc.marlonob.info/atisket/?${fragment}_id=${id}`;
+                newlink.text = 'a-tisket';
+
+                next.before(document.createTextNode(' ['));
+                next.before(newlink);
+                next.before(document.createTextNode(']'));
+            }
+        });
+    }
+
     // Discogs link rollover
     // TODO...
 
