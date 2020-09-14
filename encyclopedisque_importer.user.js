deeafc0cb42681da750530ce1c3e298973723ff2
Aurélien Mino <aurelien.mino@gmail.com>
2020-09-14 22:08:58 +0200

    Fix version

--- encyclopedisque_importer.user.js
+++ encyclopedisque_importer.user.js
@@ -1,6 +1,6 @@
 ﻿// ==UserScript==
 // @name           Import Encyclopedisque releases to MusicBrainz
-// @version        2020.09.13.1
+// @version        2020.9.13.1
 // @namespace      http://userscripts.org/users/22504
 // @description    Easily import Encyclopedisque releases into MusicBrainz
 // @downloadURL    https://raw.github.com/murdos/musicbrainz-userscripts/master/encyclopedisque_importer.user.js
