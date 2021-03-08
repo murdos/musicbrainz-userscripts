b4b9a1b31ffa172946367232faf3bd121406f5bf
Aurélien Mino <aurelien.mino@gmail.com>
2021-03-08 09:25:48 +0100

    Update version

--- mb_relationship_shortcuts.user.js
+++ mb_relationship_shortcuts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Display shortcut for relationships on MusicBrainz
 // @description    Display icon shortcut for relationships of release-group, release, recording and work: e.g. Amazon, Discogs, Wikipedia, ... links. This allows to access some relationships without opening the entity page.
-// @version        2020.3.31.1
+// @version        2021.3.8.1
 // @author         Aurelien Mino <aurelien.mino@gmail.com>
 // @licence        GPL (http://www.gnu.org/copyleft/gpl.html)
 // @downloadURL    https://raw.github.com/murdos/musicbrainz-userscripts/master/mb_relationship_shortcuts.user.js
