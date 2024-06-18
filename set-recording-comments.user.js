8bf2b04b6d92998f0e838a646f331a3cfb20fec8
Erik Levin <p.erik.d.levin@gmail.com>
2024-06-18 08:18:35 +0200

    recording-comments: Run when `tport` (or other) query parameters are present
    Fixes #563.
    
    Co-authored-by: David Kellner <52860029+kellnerd@users.noreply.github.com>

--- set-recording-comments.user.js
+++ set-recording-comments.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name           MusicBrainz: Set recording comments for a release
 // @description    Batch set recording comments from a Release page.
-// @version        2022.2.8.1
+// @version        2024.6.18.1
 // @author         Michael Wiencek
 // @license        X11
 // @namespace      790382e7-8714-47a7-bfbd-528d0caa2333
 // @downloadURL    https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/set-recording-comments.user.js
 // @updateURL      https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/set-recording-comments.user.js
-// @include        /^https?:\/\/(\w+\.)?musicbrainz\.org\/release\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}(\/disc\/\d+|$)/
+// @include        /^https?:\/\/(\w+\.)?musicbrainz\.org\/release\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}(\/disc\/\d+|(\?.+?)?$)/
 // @grant          none
 // @run-at         document-idle
 // ==/UserScript==
