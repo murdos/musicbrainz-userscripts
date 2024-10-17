7dfa34f8dae2e66ec98a0653ba367695a29a63c1
Aerozol <aerozol1@gmail.com>
2024-10-18 01:05:38 +1300

    bandcamp_importer: Harmony link auto-checks other services
    Makes the Harmony link auto-search/check the other available platforms in Harmony, saving 5 clicks every time.
    
    I'm not sure why anybody clicking this link would not want those fields checked, if they are only interested in BC data they can use this Bandcamp importer script to import.
    
    Rather than set each provider in the URL use 'category=default', which will check the defaults selected by Harmony.

--- bandcamp_importer.user.js
+++ bandcamp_importer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           Import Bandcamp releases to MusicBrainz
 // @description    Add a button on Bandcamp's album pages to open MusicBrainz release editor with pre-filled data for the selected release
-// @version        2024.9.6.1
+// @version        2024.10.17.1
 // @namespace      http://userscripts.org/users/22504
 // @downloadURL    https://raw.github.com/murdos/musicbrainz-userscripts/master/bandcamp_importer.user.js
 // @updateURL      https://raw.github.com/murdos/musicbrainz-userscripts/master/bandcamp_importer.user.js
@@ -408,7 +408,7 @@ $(document).ready(function () {
         document.querySelector('div #trackInfoInner').insertAdjacentHTML(
             'beforeend',
             `<div id="mbimport_upc" style="margin-bottom: 2em; font-size: smaller;">UPC: ${upc}<br/>
-            Import: <a href="https://harmony.pulsewidth.org.uk/release?url=${encodeURIComponent(release.url)}">Harmony</a>
+            Import: <a href="https://harmony.pulsewidth.org.uk/release?url=${encodeURIComponent(release.url)}&category=default">Harmony</a>
             | <a href="https://atisket.pulsewidth.org.uk/?upc=${upc}">a-tisket</a></div>`
         );
     }
