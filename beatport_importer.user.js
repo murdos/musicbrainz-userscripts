978e77d20a5c8870c6c01d92de11d35545224ae9
Raman Sinclair <raman@arsinclair.dev>
2025-10-20 03:58:21 +0200

    feat(beatport_importer): release the Beatport userscript rewritten in TypeScript (#711)

--- beatport_importer.user.js
+++ beatport_importer.user.js
@@ -3,9 +3,9 @@
 // @author       VxJasonxV
 // @namespace    https://github.com/murdos/musicbrainz-userscripts/
 // @description  One-click importing of releases from beatport.com/release pages into MusicBrainz
-// @version      2025.09.28
-// @downloadURL  https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/beatport_importer.user.js
-// @updateURL    https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/beatport_importer.user.js
+// @version      2025.10.20.1
+// @downloadURL  https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/dist/beatport_importer.user.js
+// @updateURL    https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/dist/beatport_importer.user.js
 // @match        https://www.beatport.com/release/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @require      lib/mbimport.js
