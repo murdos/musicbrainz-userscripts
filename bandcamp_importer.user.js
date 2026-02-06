93a2ff6046189ec691cf570c3f7ddb995d1bc3ef
Raman Sinclair <raman@arsinclair.dev>
2026-02-06 03:57:49 +0100

    fix(MBLinks): avoid matching empty cache entries (#787)
    - Improved cache validation logic in mblinks.js to ensure that URLs are present and the cache is valid before accessing it.
    - Previous logic led to some oddities when certain links would not be always displayed.

--- bandcamp_importer.user.js
+++ bandcamp_importer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Import Bandcamp releases to MusicBrainz
 // @description  Add a button on Bandcamp's album pages to open MusicBrainz release editor with pre-filled data for the selected release
-// @version      2025.12.23.1
+// @version      2026.02.06.1
 // @namespace    http://userscripts.org/users/22504
 // @downloadURL  https://raw.github.com/murdos/musicbrainz-userscripts/master/bandcamp_importer.user.js
 // @updateURL    https://raw.github.com/murdos/musicbrainz-userscripts/master/bandcamp_importer.user.js
