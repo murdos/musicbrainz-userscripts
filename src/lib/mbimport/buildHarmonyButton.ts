39f7e89006917d0366d1865a54ee79db70e41328
Raman Sinclair <raman@arsinclair.dev>
2026-05-28 03:53:40 +0200

    feat(beatport_importer): switch from atisket import to Harmony
    - also port the code to the old JS lib, as I'm planning to reuse the button in the bandcamp script as well.
    
    partially addresses #873

---
+++ src/lib/mbimport/buildHarmonyButton.ts
@@ -0,0 +1,53 @@
+interface Props {
+    barcode?: string | undefined;
+    release_url: string;
+}
+
+const styleBlock = `
+    <style>
+        .harmony-button {
+            display: flex;
+            align-items: center;
+            gap: 4px;
+            margin: 0 !important;
+            border-radius: 5px;
+            justify-content: center;
+            cursor: pointer;
+            font-family: Arial;
+            font-size: 12px !important;
+            padding: 3px 6px;
+            border: 1px solid rgba(180,180,180,0.8) !important;
+            background-color: rgba(240,240,240,0.8) !important;
+            color: #334 !important;
+            height: 26px;
+            user-select: none;
+            text-decoration: none !important;
+        }
+
+        .harmony-button:hover {
+            background-color: rgba(250,250,250,0.9) !important;
+        }
+
+        .harmony-button:active {
+            background-color: rgba(170,170,170,0.8) !important;
+        }
+    </style>
+`;
+
+export function buildHarmonyButton({ barcode, release_url }: Props): string {
+    const harmonyWithBarcodeURL = barcode
+        ? `https://harmony.pulsewidth.org.uk/release?gtin=${barcode}&category=all`
+        : `https://harmony.pulsewidth.org.uk/release?url=${encodeURIComponent(release_url)}&category=musicbrainz`;
+
+    return `
+        ${styleBlock}
+        <a
+            class="harmony-button"
+            title="Import this release into MusicBrainz using Harmony (open a new tab)" 
+            target="_blank" 
+            href="${harmonyWithBarcodeURL}"
+        >
+            <img src="https://harmony.pulsewidth.org.uk/favicon.svg" alt="Harmony icon" width="16" height="16" />
+            Import with Harmony
+        </a>`;
+}
