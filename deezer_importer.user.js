b306aacb98ac30224a7d17a9b84be89a28a89b87
chaban <225617645+chaban-mb@users.noreply.github.com>
2026-08-24 21:00:12 +0200

    chore(deezer_importer): deprecate legacy root script and add console warning

--- deezer_importer.user.js
+++ deezer_importer.user.js
@@ -1,18 +1,14 @@
 // ==UserScript==
-// @name         Import Deezer releases into MusicBrainz
+// @name         Import Deezer releases into MusicBrainz (⚠️ deprecated, no-op)
+// @description  [⚠️ script is deprecated] This version is deprecated and was converted to TypeScript. The userscript is now distributed from dist/deezer_importer.user.js. This script is a no-op now and will be removed in the future.
 // @namespace    https://github.com/murdos/musicbrainz-userscripts/
-// @description  One-click importing of releases from deezer.com into MusicBrainz. Also allows to submit their ISRCs to MusicBrainz releases.
 // @version      2026.05.31.2
 // @downloadURL  https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/dist/deezer_importer.user.js
 // @updateURL    https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/dist/deezer_importer.user.js
 // @match        https://www.deezer.com/*/album/*
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
-// @require      lib/mbimport.js
-// @require      lib/logger.js
-// @require      lib/mbimportstyle.js
 // @icon         https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/assets/images/Musicbrainz_import_logo.png
-// @grant        GM_xmlhttpRequest
-// @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
-// Dummy content to allow userscript managers to auto-upgrade users to dist/deezer_importer.user.js
+console.warn(
+    '⚠️ This version of Deezer importer is deprecated and was converted to TypeScript. If you are seeing this message, please update your userscript to: https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/dist/deezer_importer.user.js',
+);
