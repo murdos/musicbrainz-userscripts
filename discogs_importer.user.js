b42fe508b071ef0ac23ecf89cb41153d8a717089
Raman Sinclair <2878904+arsinclair@users.noreply.github.com>
2026-09-15 04:23:24 +0200

    fix: try to work around broken ViolentMonkey cache busting for bundled libs
    - for some reason VM caches the old libs aggressively and ignores the updated query string. What's more surprising is that it is only reproduced for the discogs script.
    - this PR attempts to work around that bug by updating the URL of the dependency rather than the query string.

--- discogs_importer.user.js
+++ discogs_importer.user.js
@@ -2,7 +2,7 @@
 
 // @name         Import Discogs releases to MusicBrainz
 // @description  Add a button to import Discogs releases to MusicBrainz and add links to matching MusicBrainz entities for various Discogs entities (artist,release,master,label)
-// @version      2026.9.15.2
+// @version      2026.9.15.3
 // @namespace    http://userscripts.org/users/22504
 // @downloadURL  https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/discogs_importer.user.js
 // @updateURL    https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/discogs_importer.user.js
@@ -14,7 +14,7 @@
 // @require      lib/mbimport.js
 // @require      lib/logger.js
 // @require      lib/mblinks.js?version=v2026.09.15.3
-// @require      lib/mbimportstyle.js?version=v2026.09.15.2
+// @require      https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/e07c9bf5850be6cd6a2119f66ea7769afecf1058/lib/mbimportstyle.js
 // @icon         https://metabrainz.org/static/img/projects/musicbrainz.svg
 // @grant        GM.xmlHttpRequest
 // @grant        GM_xmlhttpRequest
--- qobuz_importer.user.js
+++ qobuz_importer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Import Qobuz releases to MusicBrainz
 // @description  Add a button on Qobuz's album pages to open MusicBrainz release editor with pre-filled data for the selected release
-// @version      2026.9.15.3
+// @version      2026.9.15.4
 // @namespace    https://github.com/murdos/musicbrainz-userscripts
 // @downloadURL  https://raw.github.com/murdos/musicbrainz-userscripts/master/qobuz_importer.user.js
 // @updateURL    https://raw.github.com/murdos/musicbrainz-userscripts/master/qobuz_importer.user.js
@@ -13,7 +13,7 @@
 // @require      lib/mbimport.js?version=v2026.05.30.1
 // @require      lib/logger.js
 // @require      lib/mblinks.js?version=v2026.09.15.3
-// @require      lib/mbimportstyle.js?version=v2026.09.15.2
+// @require      https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/e07c9bf5850be6cd6a2119f66ea7769afecf1058/lib/mbimportstyle.js
 // @grant        GM.xmlHttpRequest
 // @grant        GM_xmlhttpRequest
 // @icon         https://metabrainz.org/static/img/projects/musicbrainz.svg
