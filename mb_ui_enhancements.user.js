235425257962f6ecebecbece896fe31f0a70245e
Raman Sinclair <2878904+arsinclair@users.noreply.github.com>
2026-09-16 07:57:45 +0200

    chore(mb_ui): bump version for upgrade

--- mb_ui_enhancements.user.js
+++ mb_ui_enhancements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Musicbrainz UI enhancements
 // @description  Various UI enhancements for Musicbrainz
-// @version      2026.8.26.1
+// @version      2026.9.16.1
 // @downloadURL  https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/mb_ui_enhancements.user.js
 // @updateURL    https://raw.githubusercontent.com/murdos/musicbrainz-userscripts/master/mb_ui_enhancements.user.js
 // @icon         https://metabrainz.org/static/img/projects/musicbrainz.svg
